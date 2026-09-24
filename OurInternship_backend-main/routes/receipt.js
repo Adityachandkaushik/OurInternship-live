d300fe7fd5e0b104af26da87e0b74e83657e6bd1
Aditya kaushik <165358967+Adityachandkaushik@users.noreply.github.com>
2026-09-24 11:33:54 +0530

    Remove unnecessary blank line in receipt.js

--- OurInternship_backend-main/routes/receipt.js
+++ OurInternship_backend-main/routes/receipt.js
@@ -2,7 +2,7 @@ import express from "express";
 import { downloadReceipt } from "../controllers/receiptController.js";
 
 const router = express.Router();
-
+   
 router.get("/:applicationId", downloadReceipt);
 
 export default router;
