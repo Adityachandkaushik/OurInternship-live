75effa79b056d6204b7c6b9c7992b24feda8729f
Aditya kaushik <165358967+Adityachandkaushik@users.noreply.github.com>
2026-09-25 11:24:33 +0530

    Fix formatting in userController.js

--- OurInternship_backend-main/controllers/userController.js
+++ OurInternship_backend-main/controllers/userController.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
+ 
 /* ================= REGISTER ================= */
 export const register = async (req, res) => {
   try {
